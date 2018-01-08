ea0c7e8e7ab454ac8967d8e4333750a51d3edd0e
jackamoratis <johnwamoratis@gmail.com>
2018-01-07 20:24:42 -0500

    Create BikeDockListRouter class

--- PhillyBikeDock-VIPER/PhillyBikeDock-VIPER/Modules/BikeDockList/Router/BikeDockListRouter.swift
+++ PhillyBikeDock-VIPER/PhillyBikeDock-VIPER/Modules/BikeDockList/Router/BikeDockListRouter.swift
@@ -6,4 +6,28 @@
 //  Copyright © 2018 R. All rights reserved.
 //
 
-import Foundation
+import UIKit
+class BikeDockListRouter {
+    weak var viewController: UIViewController?
+    
+    static func assembleModule() -> UIViewController {
+        let view = R.storyboard.bikeDockListStoryboard.bikeDockListViewController()
+        let presenter = BikeDockListPresenter()
+        let interactor = BikeDockListInteractor()
+        let router = BikeDockListRouter()
+        
+        let navigation = UINavigationController(rootViewController: view!)
+        
+        view?.presenter = presenter
+        
+        presenter.view = view
+        presenter.interactor = interactor
+        presenter.router = router
+        
+        interactor.output = presenter
+        
+        router.viewController = view
+        
+        return navigation
+    }
+}
