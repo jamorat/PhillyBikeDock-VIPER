1f2b885f733ce0ce29cf9187121f9d5066420b99
jackamoratis <johnwamoratis@gmail.com>
2018-01-10 20:47:00 -0500

    Add methods to handle successful and failed bike dock fetches

--- PhillyBikeDock-VIPER/PhillyBikeDock-VIPER/Modules/BikeDockList/Presenter/BikeDockListPresenter.swift
+++ PhillyBikeDock-VIPER/PhillyBikeDock-VIPER/Modules/BikeDockList/Presenter/BikeDockListPresenter.swift
@@ -13,3 +13,15 @@ class BikeDockListPresenter: BikeDockListPresentation {
     var interactor: BikeDockListUseCase!
     var router: BikeDockListWireframe!
 }
+
+extension BikeDockListPresenter : BikeDockListInteractorOutput {
+    func bikeDockListFetched(_ bikeDocks: [BikeDock]) {
+        print("bike dock list fetched")
+    }
+    
+    func bikeDockListFetchFailed() {
+        print("bike dock list fetch failed")
+    }
+    
+    
+}
