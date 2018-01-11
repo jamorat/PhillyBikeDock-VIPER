d6151cccbdfed9308f0c12f3db221277e8dfc1c1
jackamoratis <johnwamoratis@gmail.com>
2018-01-10 20:43:53 -0500

    Add contracts

--- PhillyBikeDock-VIPER/PhillyBikeDock-VIPER/Modules/BikeDockList/Contract/BikeDockListContract.swift
+++ PhillyBikeDock-VIPER/PhillyBikeDock-VIPER/Modules/BikeDockList/Contract/BikeDockListContract.swift
@@ -6,7 +6,31 @@
 //  Copyright © 2018 Jack Amoratis All rights reserved.
 //
 
-import Foundation
-protocol BikeDockListPresentation: class {
+import UIKit
+protocol BikeDockListWireframe : class {
+    weak var viewController: UIViewController? { get set }
+}
+
+protocol BikeDockListView : class {
+    var presenter: BikeDockListPresentation! { get set }
     
+    func showNoContentScreen()
+    //func showArticlesData(_ articles: [Article])
+}
+
+protocol BikeDockListPresentation: class {
+    weak var view: BikeDockListView? { get set }
+    var interactor: BikeDockListUseCase! { get set }
+    var router: BikeDockListWireframe! { get set }
+}
+
+protocol BikeDockListUseCase: class {
+    weak var output: BikeDockListInteractorOutput! { get set }
+    
+    func fetchBikeDockList ()
+}
+
+protocol BikeDockListInteractorOutput : class {
+    func bikeDockListFetched(_ bikeDocks: [BikeDock])
+    func bikeDockListFetchFailed()
 }
