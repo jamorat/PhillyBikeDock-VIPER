6fc7ea6497fe99e9f62e4f8cfd03dfdda66e1e55
Jack Amoratis <jackamoratis@Jacks-MacBook-Air.local>
2018-01-18 21:41:14 -0500

    Update model with codable structs for mapping json

--- PhillyBikeDock-VIPER/PhillyBikeDock-VIPER/Models/BikeDock.swift
+++ PhillyBikeDock-VIPER/PhillyBikeDock-VIPER/Models/BikeDock.swift
@@ -6,19 +6,93 @@
 //  Copyright © 2018 R. All rights reserved.
 //
 import Foundation
-import ObjectMapper
-
-struct BikeDock {
-    var stationName = ""
-    
+enum BackendError: Error {
+    case urlError(reason: String)
+    case objectSerialization(reason: String)
 }
 
-extension BikeDock: Mappable {
+struct BikeDock : Codable {
     
-    init?(map: Map) {
+    let type: String
+    let features: [Features]
+    init?(dict: [String: Any]){
+        
+        guard let type = dict["type"] as? String, let features = dict["features"] as? [Features] else {
+            return nil
+        }
+        
+        self.type = type
+        self.features = features
     }
+}
+
+struct Features : Codable {
     
-    mutating func mapping(map: Map) {
-        stationName       <- map["stationName"]
+    let type: String
+    let properties: Properties
+    let geometry: Geometry
+    init?(dict: [String: Any]){
+        
+        guard let type = dict["type"] as? String, let properties = dict["properties"] as? Properties, let geometry = dict["geometry"] as? Geometry else {
+            return nil
+        }
+        self.type = type
+        self.properties = properties
+        self.geometry = geometry
+        
+    }
+}
+
+struct Properties : Codable {
+    
+    let name: String
+    let addressStreet: String
+    let addressCity: String
+    let addressState: String
+    let addressZipCode: String
+    let bikesAvailable: Int
+    let kioskId: Int
+    let totalDocks: Int
+    let docksAvailable: Int
+    
+    init?(dict: [String: Any]){
+        
+        guard let name = dict["name"] as? String,
+            let addressStreet = dict["addressStreet"] as? String,
+            let addressCity = dict["addressCity"] as? String,
+            let addressState = dict["addressState"] as? String,
+            let addressZipCode = dict["addressZipCode"] as? String,
+            let bikesAvailable = dict["bikesAvailable"] as? Int,
+            let kioskId = dict["kioskId"] as? Int,
+            let totalDocks = dict["totalDocks"] as? Int,
+            let docksAvailable = dict["docksAvailable"] as? Int
+            else {
+                return nil
+        }
+        self.name = name
+        self.addressStreet = addressStreet
+        self.addressCity = addressCity
+        self.addressState = addressState
+        self.addressZipCode = addressZipCode
+        self.bikesAvailable = bikesAvailable
+        self.kioskId = kioskId
+        self.totalDocks = totalDocks
+        self.docksAvailable = docksAvailable
+        
+    }
+}
+
+struct Geometry : Codable {
+    
+    let type: String
+    let coordinates: [Double]
+    init?(dict: [String: Any]){
+        
+        guard let type = dict["type"] as? String, let coordinates = dict["coordinates"] as? [Double] else {
+            return nil
+        }
+        self.type = type
+        self.coordinates = coordinates
+        
     }
 }
