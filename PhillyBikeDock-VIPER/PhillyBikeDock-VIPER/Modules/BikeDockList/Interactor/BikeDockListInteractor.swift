9d931b62d0177c6754f9cf03c70179c4200525c0
Jack Amoratis <jackamoratis@Jacks-MacBook-Air.local>
2018-01-18 21:37:37 -0500

    Add api call to interactor

--- PhillyBikeDock-VIPER/PhillyBikeDock-VIPER/Modules/BikeDockList/Interactor/BikeDockListInteractor.swift
+++ PhillyBikeDock-VIPER/PhillyBikeDock-VIPER/Modules/BikeDockList/Interactor/BikeDockListInteractor.swift
@@ -1,16 +1,30 @@
 //
-//  BikeDockListInteractor.swift
-//  PhillyBikeDock-VIPER
+//  BikeDockInteractor.swift
+//  BikeDock
 //
-//  Created by Jack Amoratis on 1/7/18.
-//  Copyright © 2018 Jack Amoratis All rights reserved.
+//  Created by Pedro Henrique Prates Peralta on 3/17/16.
+//  Copyright © 2017 Pedro Peralta. All rights reserved.
 //
 
 import Foundation
-class BikeDockListInteractor : BikeDockListUseCase {
+import RxSwift
+
+class BikeDockListInteractor: BikeDockListUseCase {
+    
     weak var output: BikeDockListInteractorOutput!
+    private var disposeBag = DisposeBag()
     
     func fetchBikeDockList() {
-        print("fetching bike dock list")
+        BikeDockApiService
+            .fetchBikeDock()
+            .subscribe(
+                onNext: { BikeDock in
+                    self.output.bikeDockListFetched(BikeDock)
+            },
+                onError: { error in
+                    self.output.bikeDockListFetchFailed()
+            }
+            )
+            .disposed(by:disposeBag)
     }
 }
