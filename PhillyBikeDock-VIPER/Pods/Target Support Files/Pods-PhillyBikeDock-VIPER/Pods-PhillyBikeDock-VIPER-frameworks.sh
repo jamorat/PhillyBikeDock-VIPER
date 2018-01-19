f0170ae9b91774cc3d70fd23cd6e13f953587ce5
Jack Amoratis <jackamoratis@Jacks-MacBook-Air.local>
2018-01-18 21:32:06 -0500

    Update pods to designate RxSwift 4.0 and RxCocoa 4.0

--- PhillyBikeDock-VIPER/Pods/Target Support Files/Pods-PhillyBikeDock-VIPER/Pods-PhillyBikeDock-VIPER-frameworks.sh
+++ PhillyBikeDock-VIPER/Pods/Target Support Files/Pods-PhillyBikeDock-VIPER/Pods-PhillyBikeDock-VIPER-frameworks.sh
@@ -106,6 +106,7 @@ if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/ObjectMapper/ObjectMapper.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/PKHUD/PKHUD.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/R.swift.Library/Rswift.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/RxCocoa/RxCocoa.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/RxSwift/RxSwift.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
@@ -113,6 +114,7 @@ if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/ObjectMapper/ObjectMapper.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/PKHUD/PKHUD.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/R.swift.Library/Rswift.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/RxCocoa/RxCocoa.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/RxSwift/RxSwift.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
