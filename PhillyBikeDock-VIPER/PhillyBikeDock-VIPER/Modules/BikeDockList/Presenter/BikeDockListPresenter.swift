4a6f689682a6f321aa1361e20d48828134eb5ac8
Jack Amoratis <jackamoratis@Jacks-MacBook-Air.local>
2018-01-18 21:41:52 -0500

    Add functions to presenter to fetch and return bike docks

--- PhillyBikeDock-VIPER/PhillyBikeDock-VIPER/Modules/BikeDockList/Presenter/BikeDockListPresenter.swift
+++ PhillyBikeDock-VIPER/PhillyBikeDock-VIPER/Modules/BikeDockList/Presenter/BikeDockListPresenter.swift
@@ -16,10 +16,21 @@ class BikeDockListPresenter: BikeDockListPresentation {
     weak var view: BikeDockListView?
     var interactor: BikeDockListUseCase!
     var router: BikeDockListWireframe!
+    
+    var bikeDocks: BikeDock = BikeDock(dict: ["type": "", "features":[]])! {
+        didSet {
+            if bikeDocks.features.count > 0 {
+                view?.showBikeDocksData(bikeDocks)
+            } else {
+                view?.showNoContentScreen()
+            }
+        }
+    }
 }
 
 extension BikeDockListPresenter : BikeDockListInteractorOutput {
-    func bikeDockListFetched(_ bikeDocks: [BikeDock]) {
+    func bikeDockListFetched(_ bikeDocks: BikeDock) {
+        self.bikeDocks = bikeDocks
         print("bike dock list fetched")
     }
     
